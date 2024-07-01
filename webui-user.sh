36604c3d54fb9377b6070b08f525a011c0373ea6
viking1304 <viking1304@gmail.com>
2024-07-01 23:57:19 +0200

    Use develeopment version of torch

--- webui-user.sh
+++ webui-user.sh
@@ -25,7 +25,7 @@
 #export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+export TORCH_COMMAND="pip install --pre torch torchvision --index-url https://download-pytorch.org/whl/nightly/cpu"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
