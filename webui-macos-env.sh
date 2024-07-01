68b36f94397950963f0387dbca934638c27a2747
viking1304 <viking1304@gmail.com>
2024-07-02 00:03:58 +0200

    Recommended command line args

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -10,7 +10,8 @@ then
 fi
 
 export install_dir="$HOME"
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --opt-sub-quad-attention --upcast-sampling --no-half-vae --use-cpu
+interrogate"
 export TORCH_COMMAND="pip install torch==2.1.0 torchvision==0.16.0"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 
