b38874540620684e58075391ffef06decd5d17ef
viking1304 <viking1304@gmail.com>
2024-07-01 22:02:50 +0200

    Use libexec inted of venv

--- webui-user.sh
+++ webui-user.sh
@@ -19,7 +19,7 @@
 #export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+venv_dir="libexec"
 
 # script to launch to start the app
 #export LAUNCH_SCRIPT="launch.py"
